1595d1375220ae110de181884d17528d1acc084c
Javier Alaves <jayalaves@gmail.com>
2023-07-21 20:22:08 +0200

    Delivery details

---
+++ CupcakeCorner/CheckoutView.swift
@@ -0,0 +1,23 @@
+//
+//  CheckoutView.swift
+//  CupcakeCorner
+//
+//  Created by Javier Alaves on 21/7/23.
+//
+
+import SwiftUI
+
+struct CheckoutView: View {
+    
+    @ObservedObject var order: Order
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CheckoutView_Previews: PreviewProvider {
+    static var previews: some View {
+        CheckoutView(order: Order())
+    }
+}
