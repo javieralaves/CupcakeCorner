a72fee2f299d2fe1dc765f0fd4517997e4a7c9dc
Javier Alaves <jayalaves@gmail.com>
2023-07-22 09:21:17 +0200

    Networking

--- CupcakeCorner/CheckoutView.swift
+++ CupcakeCorner/CheckoutView.swift
@@ -11,6 +11,9 @@ struct CheckoutView: View {
     
     @ObservedObject var order: Order
     
+    @State private var confirmationMessage = ""
+    @State private var showingConfirmation = false
+    
     var body: some View {
         ScrollView {
             VStack {
@@ -26,12 +29,49 @@ struct CheckoutView: View {
                 Text("Your total is \(order.cost, format: .currency(code: "USD"))")
                     .font(.title)
                 
-                Button("Place Order", action: { })
+                Button("Place Order", action: {
+                    // Need to place the async function inside a Task with the await keyboard because button expects sync functionality by default
+                    Task {
+                        await placeOrder()
+                    }
+                })
                     .padding()
             }
         }
         .navigationTitle("Check out")
         .navigationBarTitleDisplayMode(.inline)
+        .alert("Thank you!", isPresented: $showingConfirmation) {
+            Button("OK") { }
+        } message: {
+            Text(confirmationMessage)
+        }
+    }
+    
+    func placeOrder() async {
+        // Encode the data
+        guard let encoded = try? JSONEncoder().encode(order) else {
+            print("Failed to encode order")
+            return
+        }
+        
+        // Tell Swift how to send the data
+        // Through a URLRequest object, configured to send our JSON data using a POST
+        // Using a test server
+        let url = URL(string: "https://reqres.in/api/cupcakes")! // exclamation to bypass optional
+        var request = URLRequest(url: url)
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = "POST"
+        
+        do {
+            let (data, _) = try await URLSession.shared.upload(for: request, from: encoded)
+            
+            // Handle the result
+            let decodedOrder = try JSONDecoder().decode(Order.self, from: data)
+            confirmationMessage = "Your order for \(decodedOrder.quantity)x \(Order.types[decodedOrder.type].lowercased()) cupcakes is on its way!"
+            showingConfirmation = true
+        } catch {
+            print("Checkout failed")
+        }
     }
 }
 
